8fbf66ee270103f55e57c24d8aabb514560edaad
adamsoja1 <adamek0222@gmail.com>
2023-05-24 02:04:53 +0200

    Added API_URL variable, added docker file for docker-compose run

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import React, { useState, useEffect } from 'react';
+import { API_URL } from './API_CONFIG';
 
 function App() {
+  const [record, setRecord] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(res => setRecord(res))
+      .catch(error => console.error(error));
+  }, []);
+
+
+
   return (
     <div>
-      <h1>Test22</h1>
+      {record.length > 0 ? (
+        record.map(rec => (
+          <div key={rec.id}>
+            <h3>{rec.id}</h3>
+            <h3>{rec.audio_file}</h3>
+          </div>
+        ))
+      ) : (
+        <h3>Loading...</h3>
+      )}
+      <h2>Test dockera</h2>
     </div>
-  )
+  );
 }
 
 export default App;
